877983a649da6ec7d23c256f4d9d2fc8555bc777
Florian G. Pflug <fgp@phlo.org>
2013-03-16 23:54:43 +0100

    Used a fixed directory instead of a temporary one.

--- lvpeek.rb
+++ lvpeek.rb
@@ -2,6 +2,8 @@
 require 'fileutils'
 require 'tmpdir'
 
+LVPEEK_ROOT = "/var/lvpeek"
+
 module OPTS
   OPTIONS = [
     { :id => "mode",
@@ -82,43 +84,29 @@ Kernel.syscall(272, 0x00020000) # unshare(CLONE_NEWNS)
 
 case OPTS::MODE
   when :mount
-    LVPEEK_ROOT = Dir.mktmpdir("lvpeek_", "/tmp")
-    SNAPSHOT_NAME = File.basename(LVPEEK_ROOT)
+    SNAPSHOT_NAME = Time::now.strftime("lvpeek-%Y%m%d-%H%M%S-") + ("%06d" % rand(1000000))
     LVPEEK_CGROUP = "/" + OPTS::VOLUME_GROUP + "/" + OPTS::LOGICAL_VOLUME + "/" + SNAPSHOT_NAME
-    LVPEEK_HIERARCHY = LVPEEK_ROOT + "/lvpeek"
-    LVPEEK_MOUNTPOINT = LVPEEK_ROOT + "/mnt"
-
-    # Create directories
-    Dir.mkdir(LVPEEK_HIERARCHY)
-    Dir.mkdir(LVPEEK_MOUNTPOINT)
 
     # Create snapshot-specific cgroup in lvpeek hierarchy and assign current task.
     # Register release_agent, which takes care of removing the cgroup and the snapshot
     # once the snapshot-specific cgroup is empty.
-    system(*(%w(mount -n -t cgroup -o none,name=lvpeek cgroup) + [LVPEEK_HIERARCHY]))
-    File::write(LVPEEK_HIERARCHY + "/release_agent", File.dirname(File.expand_path($PROGRAM_NAME)) + "/lvpeek_release_agent.sh")
-    FileUtils.mkdir_p LVPEEK_HIERARCHY + LVPEEK_CGROUP
-    File::write LVPEEK_HIERARCHY + LVPEEK_CGROUP + "/cgroup.procs", "0"
-    File::write LVPEEK_HIERARCHY + LVPEEK_CGROUP + "/notify_on_release", "1"
-    system(*(%w(umount -n) + [LVPEEK_HIERARCHY])) if defined? LVPEEK_HIERARCHY
+    system(*(%w(mount -n -t cgroup -o none,name=lvpeek cgroup) + [LVPEEK_ROOT]))
+    File::write(LVPEEK_ROOT + "/release_agent", File.dirname(File.expand_path($PROGRAM_NAME)) + "/lvpeek_release_agent.sh")
+    FileUtils.mkdir_p LVPEEK_ROOT + LVPEEK_CGROUP
+    File::write LVPEEK_ROOT + LVPEEK_CGROUP + "/cgroup.procs", "0"
+    File::write LVPEEK_ROOT + LVPEEK_CGROUP + "/notify_on_release", "1"
+    system(*(%w(umount -n) + [LVPEEK_ROOT]))
+    
+    puts File::read("/proc/self/cgroup")
 
  when :cleanup
    VOLUME_GROUP, LOGICAL_VOLUME, SNAPSHOT_NAME = *OPTS::LVPEEK_CGROUP.split(/\//)[1..-1]
-   LVPEEK_ROOT = "/tmp/" + SNAPSHOT_NAME
-
-   LVPEEK_MOUNTPOINT = LVPEEK_ROOT + "/mnt"
-   LVPEEK_HIERARCHY = LVPEEK_ROOT + "/lvpeek"
 
    # Remove snapshot-specific cgroup
-   system(*(%w(mount -n -t cgroup -o none,name=lvpeek cgroup) + [LVPEEK_HIERARCHY]))
-   Dir.rmdir LVPEEK_HIERARCHY + OPTS::LVPEEK_CGROUP
-   system(*(%w(umount -n) + [LVPEEK_HIERARCHY])) if defined? LVPEEK_HIERARCHY
+   system(*(%w(mount -n -t cgroup -o none,name=lvpeek cgroup) + [LVPEEK_ROOT]))
+   Dir.rmdir LVPEEK_ROOT + OPTS::LVPEEK_CGROUP
+   system(*(%w(umount -n) + [LVPEEK_ROOT]))
    
-   # Remove directories
-   Dir.rmdir LVPEEK_MOUNTPOINT if File::directory? LVPEEK_MOUNTPOINT
-   Dir.rmdir LVPEEK_HIERARCHY if File::directory? LVPEEK_HIERARCHY
-   Dir.rmdir LVPEEK_ROOT if File::directory? LVPEEK_ROOT
-
    # Log success
    puts "#{Time.now}: Successfully cleaned up #{SNAPSHOT_NAME}"
 end
